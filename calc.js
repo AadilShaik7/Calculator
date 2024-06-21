d9aad946fa59527eed1ae369a0210f6e9a86ce4a
AgentAadil <aadil.chotu@gmail.com>
2024-06-20 21:45:42 -0500

    Added Event Listener

---
+++ calc.js
@@ -0,0 +1,55 @@
+const button9= document.getElementById("button9");
+button9.addEventListener("click",handleClickFunc);
+
+const button8= document.getElementById("button8");
+button8.addEventListener("click",handleClickFunc);
+
+const button7= document.getElementById("button7");
+button7.addEventListener("click",handleClickFunc);
+
+const button6= document.getElementById("button6");
+button6.addEventListener("click",handleClickFunc);
+
+const button5= document.getElementById("button5");
+button5.addEventListener("click",handleClickFunc);
+
+const button4= document.getElementById("button4");
+button4.addEventListener("click",handleClickFunc);
+
+const button3= document.getElementById("button3");
+button3.addEventListener("click",handleClickFunc);
+
+const button2= document.getElementById("button2");
+button2.addEventListener("click",handleClickFunc);
+
+const button1= document.getElementById("button1");
+button1.addEventListener("click",handleClickFunc);
+
+const button0= document.getElementById("button0");
+button0.addEventListener("click",handleClickFunc);
+
+const buttonmod= document.getElementById("buttonmod");
+buttonmod.addEventListener("click",handleClickFunc);
+
+const buttonmul= document.getElementById("buttonmul");
+buttonmul.addEventListener("click",handleClickFunc);
+
+const buttonmin= document.getElementById("buttonmin");
+buttonmin.addEventListener("click",handleClickFunc);
+
+const buttonplus= document.getElementById("buttonplus");
+buttonplus.addEventListener("click",handleClickFunc);
+
+const buttonequals= document.getElementById("buttonequals");
+buttonequals.addEventListener("click",handleClickFunc);
+
+const buttonAC= document.getElementById("buttonAC");
+buttonAC.addEventListener("click",handleClickFunc);
+
+
+
+function handleClickFunc(event){
+    console.log(event.srcElement.innerHTML);
+    
+
+}
